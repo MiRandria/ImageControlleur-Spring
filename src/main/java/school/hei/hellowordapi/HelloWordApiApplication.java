566b99b4242777bcd1352c71efe7380a62bdb441
MiRandria <hei.minosoa@gmail.com>
2022-08-03 23:39:19 +0300

    feat : Image Spring

---
+++ src/main/java/school/hei/hellowordapi/HelloWordApiApplication.java
@@ -0,0 +1,14 @@
+package school.hei.hellowordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HelloWordApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWordApiApplication.class, args);
+    }
+
+}
